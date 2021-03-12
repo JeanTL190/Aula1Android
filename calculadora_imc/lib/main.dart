54dc9fc4605169eb399093913913c27d6c0808f1
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2021-03-12 17:26:59 -0300

    Texto falando quanto deve perder ou ganhar

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -23,11 +23,13 @@ class _HomeState extends State<Home> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _infoText = "Informe seus dados!";
+  String _idealText = "";
   void _resetField() {
     pesoController.text = "";
     altController.text = "";
     setState(() {
       _infoText = "Informe seus dados!";
+      _idealText = "";
       _formKey = GlobalKey<FormState>();
     });
   }
@@ -37,18 +39,27 @@ class _HomeState extends State<Home> {
       double peso = double.parse(pesoController.text);
       double alt = double.parse(altController.text) / 100;
       double imc = peso / (alt * alt);
+      double minIdeal = 18.6 * alt * alt - peso;
+      double maxIdeal = peso - 24.8 * alt * alt;
+      print (peso*1.0-maxIdeal*1.0);
       if (imc < 18.6) {
-        _infoText = "Abaixo do Peso! (${imc.toStringAsPrecision(2)})";
+        _infoText = "Abaixo do Peso! (${imc.toStringAsFixed(2)})";
+        _idealText = "Ganhe ${minIdeal.toStringAsFixed(2)}kg para chegar ao peso ideal!";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso Ideal! (${imc.toStringAsPrecision(2)})";
+        _infoText = "Peso Ideal! (${imc.toStringAsFixed(2)})";
+        _idealText = "Meus parabéns! Mantenha entre ${(peso*1.0+minIdeal*1.0).toStringAsFixed(2)}kg e ${(peso*1.0-maxIdeal*1.0).toStringAsFixed(2)}kg para o peso ideal!";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente Acima do Peso! (${imc.toStringAsPrecision(2)})";
+        _infoText = "Levemente Acima do Peso! (${imc.toStringAsFixed(2)})";
+        _idealText = "Perca ${maxIdeal.toStringAsFixed(2)}kg para chegar ao peso ideal!";
       } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade Grau I! (${imc.toStringAsPrecision(2)})";
+        _infoText = "Obesidade Grau I! (${imc.toStringAsFixed(2)})";
+        _idealText = "Perca ${maxIdeal.toStringAsFixed(2)}kg para chegar ao peso ideal!";
       } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade Grau II! (${imc.toStringAsPrecision(2)})";
+        _infoText = "Obesidade Grau II! (${imc.toStringAsFixed(2)})";
+        _idealText = "Perca ${maxIdeal.toStringAsFixed(2)}kg para chegar ao peso ideal!";
       } else if (imc >= 39.9) {
-        _infoText = "Obesidade Grau III! (${imc.toStringAsPrecision(2)})";
+        _infoText = "Obesidade Grau III! (${imc.toStringAsFixed(2)})";
+        _idealText = "Perca ${maxIdeal.toStringAsFixed(2)}kg para chegar ao peso ideal!";
       }
     });
   }
@@ -109,8 +120,8 @@ class _HomeState extends State<Home> {
                 child: Container(
                   height: 50.0,
                   child: RaisedButton(
-                    onPressed: (){
-                      if(_formKey.currentState.validate()){
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
                         _calcularImc();
                       }
                     },
@@ -129,6 +140,14 @@ class _HomeState extends State<Home> {
                 _infoText,
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.blue, fontSize: 25.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text(
+                  _idealText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 25.0),
+                ),
               )
             ],
           ),
